bed52174f1b9f10a8399a2f0313cdd6b9f26c55d
yossrSafaeProjet <148536759+yossrSafaeProjet@users.noreply.github.com>
2024-08-30 11:10:14 +0200

    aaa

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,10 @@
 
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import FileTree from './components/FileTree';
 import MarkdownPreview from './components/MarkdownPreview';
 
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 
 import BibliothequeBloc from './blocs/BibliothequeBloc';
 import BlocPersonlise from './blocs/BlocPersonlise';
@@ -20,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<FileTree />} />
         <Route path="/preview" element={<MarkdownPreview />} />
-      <Route path="/" element={<BibliothequeBloc />} />
+      <Route path="/biblio" element={<BibliothequeBloc />} />
       <Route path="/img" element={<BiblioImg />} />
       <Route path="/ajouter" element={<BlocPersonlise />} />
       <Route path="/r" element={<RaccourciClavier blocks={[1,"aa","aa"]}/>} />
